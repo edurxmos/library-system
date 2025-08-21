4fad152c2d27e4ec9165dd6f2187b339e232d88a
edurxmos <eduardoramos15975@gmail.com>
2025-08-21 08:55:07 -0300

    feat: implemented the GET all Books endpoint using pagination

--- src/main/java/com/eduardo/library_system/controllers/BookController.java
+++ src/main/java/com/eduardo/library_system/controllers/BookController.java
@@ -1,6 +1,10 @@
 package com.eduardo.library_system.controllers;
 
+import com.eduardo.library_system.dtos.book.BookResponse;
 import com.eduardo.library_system.services.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +20,7 @@ public class BookController {
     }
 
     @GetMapping
-    public String test() {
-        return "hello world!";
+    public ResponseEntity<Page<BookResponse>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(bookService.findAll(pageable));
     }
 }
--- src/main/java/com/eduardo/library_system/services/BookService.java
+++ src/main/java/com/eduardo/library_system/services/BookService.java
@@ -1,8 +1,12 @@
 package com.eduardo.library_system.services;
 
+import com.eduardo.library_system.dtos.book.BookResponse;
 import com.eduardo.library_system.mappers.BookMapper;
 import com.eduardo.library_system.repositories.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
@@ -13,6 +17,11 @@ public class BookService {
     public BookService(BookRepository bookRepository, BookMapper bookMapper) {
         this.bookRepository = bookRepository;
         this.bookMapper = bookMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<BookResponse> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).map(x -> bookMapper.toResponse(x));
     }
 
 }
