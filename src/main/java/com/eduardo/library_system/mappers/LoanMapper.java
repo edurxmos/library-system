f114d29f7cfb4c88e0db37a8a7880bb7e31c2011
edurxmos <eduardoramos15975@gmail.com>
2025-08-28 15:25:43 -0300

    refactor: changes to toEntity and toResponse methods in LoanMapper

--- src/main/java/com/eduardo/library_system/mappers/LoanMapper.java
+++ src/main/java/com/eduardo/library_system/mappers/LoanMapper.java
@@ -1,23 +1,23 @@
 package com.eduardo.library_system.mappers;
 
-
-import com.eduardo.library_system.dtos.loan.LoanRequest;
 import com.eduardo.library_system.dtos.loan.LoanResponse;
+import com.eduardo.library_system.entities.Book;
 import com.eduardo.library_system.entities.Loan;
+import com.eduardo.library_system.entities.Student;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoanMapper {
 
-    public Loan toEntity(LoanRequest request) {
+    public Loan toEntity(Student student, Book book) {
         Loan entity = new Loan();
-        entity.setStudentId(request.studentId());
-        entity.setBookId(request.bookId());
+        entity.setStudent(student);
+        entity.setBook(book);
         return entity;
     }
 
     public LoanResponse toResponse(Loan entity) {
-        return new LoanResponse(entity.getId(), entity.getStudentId(), entity.getBookId(),
+        return new LoanResponse(entity.getId(), entity.getStudent().getId(), entity.getBook().getId(),
                 entity.getLoanDate(), entity.getReturnDate());
     }
 
