c7c47e34aeb1f173e82fcf4417173096b7c755a7
edurxmos <eduardoramos15975@gmail.com>
2025-08-28 17:22:58 -0300

    update data seeding with loans

--- src/main/resources/db/migration/R__seed_test_data.sql
+++ src/main/resources/db/migration/R__seed_test_data.sql
@@ -8,3 +8,9 @@ INSERT INTO book (title, author, available) VALUES ('Dom Casmurro', 'Machado de 
 INSERT INTO book (title, author, available) VALUES ('Clean Code', 'Robert C. Martin', 1);
 INSERT INTO book (title, author, available) VALUES ('1984', 'George Orwell', 0);
 INSERT INTO book (title, author, available) VALUES ('O Hobbit', 'J.R.R. Tolkien', 1);
+
+INSERT INTO loan (student_id, book_id, loan_date, return_date, loaned) VALUES (1, 1, '2025-08-01', '2025-08-08', TRUE);
+INSERT INTO loan (student_id, book_id, loan_date, return_date, loaned) VALUES (2, 2, '2025-08-05', '2025-08-12', TRUE);
+INSERT INTO loan (student_id, book_id, loan_date, return_date, loaned) VALUES (3, 3, '2025-08-10', '2025-08-17', TRUE);
+INSERT INTO loan (student_id, book_id, loan_date, return_date, loaned) VALUES (4, 4, '2025-08-12', '2025-08-19', FALSE);
+INSERT INTO loan (student_id, book_id, loan_date, return_date, loaned) VALUES (1, 5, '2025-08-15', '2025-08-22', TRUE);
