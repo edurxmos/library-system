69f8b74a0cdcb235cfed52132229caa0baa5711e
edurxmos <eduardoramos15975@gmail.com>
2025-08-29 18:40:33 -0300

    feat: DataBaseException

---
+++ src/main/java/com/eduardo/library_system/services/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.eduardo.library_system.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+}
