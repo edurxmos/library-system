7cfccdeac6c127bdf0928853262b90dbfc8b7be9
edurxmos <eduardoramos15975@gmail.com>
2025-08-29 17:44:23 -0300

    refactor: update LoanResponse

--- src/main/java/com/eduardo/library_system/dtos/loan/LoanResponse.java
+++ src/main/java/com/eduardo/library_system/dtos/loan/LoanResponse.java
@@ -1,6 +1,9 @@
 package com.eduardo.library_system.dtos.loan;
 
+import com.eduardo.library_system.dtos.book.BookMinResponse;
+import com.eduardo.library_system.dtos.student.StudentMinResponse;
+
 import java.time.LocalDate;
 
-public record LoanResponse(Long id, Long studentId, Long bookId, LocalDate loanDate, LocalDate returnDate) {
+public record LoanResponse(Long id, StudentMinResponse student, BookMinResponse book, LocalDate loanDate, LocalDate returnDate) {
 }
