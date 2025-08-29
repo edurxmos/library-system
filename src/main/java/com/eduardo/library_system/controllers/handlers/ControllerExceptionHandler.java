649843867da8b4e5d3feab30c1c41c9505c12fe0
edurxmos <eduardoramos15975@gmail.com>
2025-08-29 19:59:33 -0300

    feat: handler DataBaseException

--- src/main/java/com/eduardo/library_system/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/eduardo/library_system/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.eduardo.library_system.controllers.handlers;
 
 import com.eduardo.library_system.dtos.CustomError;
+import com.eduardo.library_system.services.exceptions.DataBaseException;
 import com.eduardo.library_system.services.exceptions.NotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -18,6 +19,13 @@ public class ControllerExceptionHandler {
         HttpStatus status = HttpStatus.NOT_FOUND;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<CustomError> dataBaseException(DataBaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status.value()).body(err);
     }
 
 }
