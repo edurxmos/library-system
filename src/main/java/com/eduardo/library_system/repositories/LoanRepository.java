88fc7d77d2e9740a05eded802b8f63dd65e64038
edurxmos <eduardoramos15975@gmail.com>
2025-09-23 21:26:35 -0300

    feat: implement native query to fetch active loans in LoanRepository

--- src/main/java/com/eduardo/library_system/repositories/LoanRepository.java
+++ src/main/java/com/eduardo/library_system/repositories/LoanRepository.java
@@ -1,6 +1,7 @@
 package com.eduardo.library_system.repositories;
 
 import com.eduardo.library_system.entities.Loan;
+import com.eduardo.library_system.projections.LoanActiveProjections;
 import com.eduardo.library_system.projections.LoanUserProjections;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,5 +17,12 @@ public interface LoanRepository extends JpaRepository<Loan, Long> {
             "JOIN book b ON l.book_id = b.id " +
             "WHERE l.student_id = :studentId")
     public Page<LoanUserProjections> findLoansByStudent(@Param("studentId") Long studentId, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "SELECT l.id, s.name, s.email, b.title, b.author, l.loan_date AS loanDate, l.return_date AS loanReturn  " +
+            "FROM loan l " +
+            "JOIN student s ON l.student_id = s.id " +
+            "JOIN book b ON l.book_id = b.id " +
+            "WHERE l.loaned = TRUE")
+    public Page<LoanActiveProjections> findActiveLoans(Pageable pageable);
 
 }
