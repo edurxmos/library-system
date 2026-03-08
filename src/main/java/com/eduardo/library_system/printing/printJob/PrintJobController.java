7d0095926c430823c9cb262fa1429cf9170e8025
edurxmos <eduardoramos15975@gmail.com>
2026-03-08 15:40:02 -0300

    feat: methods list all PrintJobs and insert PrintJob

--- src/main/java/com/eduardo/library_system/printing/printJob/PrintJobController.java
+++ src/main/java/com/eduardo/library_system/printing/printJob/PrintJobController.java
@@ -1,9 +1,31 @@
 package com.eduardo.library_system.printing.printJob;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eduardo.library_system.printing.printJob.dtos.PrintJobRequest;
+import com.eduardo.library_system.printing.printJob.dtos.PrintJobResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/print")
 public class PrintJobController {
+
+    private final PrintJobService printJobService;
+
+    public PrintJobController(PrintJobService printJobService) {
+        this.printJobService = printJobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<PrintJobResponse>> list(Pageable pageable) {
+        return ResponseEntity.ok(printJobService.list(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<PrintJobResponse> insert(@RequestBody PrintJobRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(printJobService.insert(request));
+    }
+
 }
--- src/main/java/com/eduardo/library_system/printing/printJob/PrintJobService.java
+++ src/main/java/com/eduardo/library_system/printing/printJob/PrintJobService.java
@@ -1,7 +1,33 @@
 package com.eduardo.library_system.printing.printJob;
 
+import com.eduardo.library_system.printing.printJob.dtos.PrintJobMapper;
+import com.eduardo.library_system.printing.printJob.dtos.PrintJobRequest;
+import com.eduardo.library_system.printing.printJob.dtos.PrintJobResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PrintJobService {
+
+    private final PrintJobRepository printJobRepository;
+    private final PrintJobMapper printJobMapper;
+
+    public PrintJobService(PrintJobRepository printJobRepository, PrintJobMapper printJobMapper) {
+        this.printJobRepository = printJobRepository;
+        this.printJobMapper = printJobMapper;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<PrintJobResponse> list(Pageable pageable) {
+        return printJobRepository.findAll(pageable).map(x -> printJobMapper.toResponse(x));
+    }
+
+    @Transactional
+    public PrintJobResponse insert(PrintJobRequest request) {
+        PrintJob entity = printJobRepository.save(printJobMapper.toEntity(request));
+        return printJobMapper.toResponse(entity);
+    }
+
 }
